747062da3c5682ebba0b10ef1d506996eb9f8c6e
gwill1337 <gwyllig@gmail.com>
2026-09-25 01:56:20 +0300

    fix

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,2 +1,2 @@
-export const API = import.meta.env.VITE_API_URL || "http://localhost:30080";
+export const API = import.meta.env.VITE_API_URL ?? "";
 export const API_VERSION = import.meta.env.VITE_API_VER || "/api/v1";
